4c6cc8932cab70e191fecbf608362007e354860b
David Reagan <dmreagan@iu.edu>
2018-07-11 18:17:20 -0400

    Refactor shader string. Create own lineWidthNVC value.

--- vtk-shaders-3.py
+++ vtk-shaders-3.py
@@ -25,46 +25,51 @@ actor.GetProperty().SetLineWidth(11)
 if case == 2:
     actor.GetProperty().SetRenderLinesAsTubes(1)
 if case == 3:
-    mapper.SetGeometryShaderCode(
-        "//VTK::System::Dec\n"
-        "//VTK::PositionVC::Dec\n"
-        "//VTK::PrimID::Dec\n"
-        "//VTK::Color::Dec\n"
-        "//VTK::Normal::Dec\n"
-        "//VTK::Light::Dec\n"
-        "//VTK::TCoord::Dec\n"
-        "//VTK::Picking::Dec\n"
-        "//VTK::DepthPeeling::Dec\n"
-        "//VTK::Clip::Dec\n"
-        "//VTK::Output::Dec\n"
-        "uniform vec2 lineWidthNVC;\n"
-        "layout(lines) in;\n"
-        "layout(triangle_strip, max_vertices = 4) out;\n"
-        "void main() {\n"
-        "   vec2 normal = normalize(\n"
-        "       gl_in[1].gl_Position.xy/gl_in[1].gl_Position.w - gl_in[0].gl_Position.xy/gl_in[0].gl_Position.w);\n"
-        "   normal = vec2(-1.0*normal.y,normal.x);\n"
-        "   for (int j = 0; j < 4; j++){\n"
-        "       int i = j/2;\n"
-        "       //VTK::PrimID::Impl\n"
-        "       //VTK::Clip::Impl\n"
-        "       //VTK::Color::Impl\n"
-        "       //VTK::Normal::Impl\n"
-        "       //VTK::Light::Impl\n"
-        "       //VTK::TCoord::Impl\n"
-        "       //VTK::DepthPeeling::Impl\n"
-        "       //VTK::Picking::Impl\n"
-        "       //VTK::PositionVC::Impl\n"
-        "       gl_Position = vec4(\n"
-        "           gl_in[i].gl_Position.xy + (lineWidthNVC*normal)*((j+1)%2 - 0.5)*gl_in[i].gl_Position.w, gl_in[i].gl_Position.z, gl_in[i].gl_Position.w);\n"
-        "       EmitVertex();\n"
-        "   }\n"
-        "   EndPrimitive();\n"
-        "}\n"
-        )
+    mapper.SetGeometryShaderCode("""
+        //VTK::System::Dec
+        //VTK::PositionVC::Dec
+        //VTK::PrimID::Dec
+        //VTK::Color::Dec
+        //VTK::Normal::Dec
+        //VTK::Light::Dec
+        //VTK::TCoord::Dec
+        //VTK::Picking::Dec
+        //VTK::DepthPeeling::Dec
+        //VTK::Clip::Dec
+        //VTK::Output::Dec
+        uniform vec2 lineWidthNVC;
+        layout(lines) in;
+        layout(triangle_strip, max_vertices = 4) out;
+        void main() {
+            int lineWidth = 10;
+            vec2 lineWidthNVC2 = vec2(2.0*lineWidth/500, 2.0*lineWidth/500);
+            vec2 normal = normalize(
+                gl_in[1].gl_Position.xy/gl_in[1].gl_Position.w - gl_in[0].gl_Position.xy/gl_in[0].gl_Position.w);
+            normal = vec2(-1.0*normal.y,normal.x);
+            for (int j = 0; j < 4; j++){
+                int i = j/2;
+                //VTK::PrimID::Impl
+                //VTK::Clip::Impl
+                //VTK::Color::Impl
+                //VTK::Normal::Impl
+                //VTK::Light::Impl
+                //VTK::TCoord::Impl
+                //VTK::DepthPeeling::Impl
+                //VTK::Picking::Impl
+                //VTK::PositionVC::Impl
+                gl_Position = vec4(
+                    //gl_in[i].gl_Position.xy + (lineWidthNVC*normal)*((j+1)%2 - 0.5)*gl_in[i].gl_Position.w, gl_in[i].gl_Position.z, gl_in[i].gl_Position.w);
+                    //gl_in[i].gl_Position.xy + (vec2(0.01, 0.01)*normal)*((j+1)%2 - 0.5)*gl_in[i].gl_Position.w, gl_in[i].gl_Position.z, gl_in[i].gl_Position.w);
+                    gl_in[i].gl_Position.xy + (lineWidthNVC2*normal)*((j+1)%2 - 0.5)*gl_in[i].gl_Position.w, gl_in[i].gl_Position.z, gl_in[i].gl_Position.w);
+                EmitVertex();
+            }
+            EndPrimitive();
+        }
+    """)
 
 renderer = vtk.vtkRenderer()
 renderWindow = vtk.vtkRenderWindow()
+renderWindow.SetSize(500, 500)
 renderWindow.AddRenderer(renderer)
 renderWindowInteractor = vtk.vtkRenderWindowInteractor()
 renderWindowInteractor.SetRenderWindow(renderWindow)
